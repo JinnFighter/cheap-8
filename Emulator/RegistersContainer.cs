63416003af84de540cd17bbfe0e918cd8ad35dbd
JinnFighter <chernayamolnia@yandex.ru>
2022-02-10 23:29:30 +0300

    add: RegistersContainer

---
+++ Emulator/RegistersContainer.cs
@@ -0,0 +1,25 @@
+﻿namespace Emulator
+{
+    public class RegistersContainer
+    {
+        private byte[] _vRegisters;
+        private ushort _iRegister;
+
+        public RegistersContainer()
+        {
+            _vRegisters = new byte[16];
+        }
+
+        public void WriteRegularRegister(int index, byte info)
+        {
+            if(index >= 0 && index < _vRegisters.Length - 1)
+            {
+                _vRegisters[index] = info;
+            }
+        }
+
+        public void WriteVFRegister(byte info) => _vRegisters[15] = info;
+
+        public void WriteIRegister(ushort info) => _iRegister = info;
+    }
+}
