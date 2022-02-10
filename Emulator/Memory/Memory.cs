719c1875888798e119d8d52870fd8e05f11c6ef8
JinnFighter <chernayamolnia@yandex.ru>
2022-02-10 23:01:41 +0300

    add: Memory class

---
+++ Emulator/Memory/Memory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulator.Memory
+{
+    public class Memory
+    {
+        private readonly byte[] _memoryBytes;
+        private readonly int _internalMemorySize;
+
+        public Memory()
+        {
+            _memoryBytes = new byte[4096];
+            _internalMemorySize = 512;
+        }
+
+        public byte GetIndex(int index) => _memoryBytes[index];
+
+        public void WriteInternal(int index, byte info)
+        {
+            if(index >= 0 && index < _internalMemorySize)
+            {
+                _memoryBytes[index] = info;
+            }
+        }
+
+        public void WriteCommon(int index, byte info)
+        {
+            if (index >= _internalMemorySize && index < _memoryBytes.Length)
+            {
+                _memoryBytes[index] = info;
+            }
+        }
+    }
+}
