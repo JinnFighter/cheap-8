2bb394450e956f23364d46a9054535bc6b1bed85
JinnFighter <chernayamolnia@yandex.ru>
2022-02-10 23:42:05 +0300

    add: AddressStack

---
+++ Emulator/AddressStack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Emulator
+{
+    public class AddressStack
+    {
+        private readonly Stack<ushort> _addresses;
+
+        public ushort TopAddress { get => _addresses.Peek(); }
+
+        public AddressStack()
+        {
+            _addresses = new Stack<ushort>(16);
+        }
+
+        public void PushAddress(ushort address) => _addresses.Push(address);
+
+        public ushort PopAddress() => _addresses.Pop();
+    }
+}
