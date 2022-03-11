200a529ecf8a990f748085a94b0eff7a8ca26944
JinnFighter <chernayamolnia@yandex.ru>
2022-03-11 12:02:31 +0300

    add Runtime

---
+++ Emulator/Runtime.cs
@@ -0,0 +1,37 @@
+﻿namespace Emulator
+{
+    public class Runtime
+    {
+        private bool _isActive;
+
+        public Runtime()
+        {
+
+        }
+
+        public void Run()
+        {
+            while(_isActive)
+            {
+                FetchInstruction();
+                DecodeInstruction();
+                ExecuteInstruction();
+            }
+        }
+
+        private void FetchInstruction()
+        {
+
+        }
+
+        private void DecodeInstruction()
+        {
+
+        }
+
+        private void ExecuteInstruction()
+        {
+
+        }
+    }
+}
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Main
+﻿using Emulator;
+
+namespace Main
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-
+            var runtime = new Runtime();
+            runtime.Run();
         }
     }
 }
