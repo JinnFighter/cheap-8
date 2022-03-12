b7c92a6acff5fe84694f09bfda42165025b7d13b
JinnFighter <chernayamolnia@yandex.ru>
2022-03-12 15:28:42 +0300

    add: DisplayInstruction

---
+++ Emulator/Instructions/DisplayInstruction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Emulator.Instructions
+{
+    internal class DisplayInstruction : IInstruction
+    {
+        private readonly RegistersContainer _registersContainer;
+        private readonly Display _display;
+
+
+        public DisplayInstruction(RegistersContainer registersContainer, Display display)
+        {
+            _registersContainer = registersContainer;
+            _display = display;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Draw!");
+        }
+    }
+}
