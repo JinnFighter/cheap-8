339b4a0b034834f0439b86c697baaac49417c062
JinnFighter <chernayamolnia@yandex.ru>
2022-03-12 15:16:45 +0300

    add: SetIRegisterInstruction

---
+++ Emulator/Instructions/SetIRegisterInstruction.cs
@@ -0,0 +1,20 @@
+﻿namespace Emulator.Instructions
+{
+    internal class SetIRegisterInstruction : IInstruction
+    {
+        private readonly RegistersContainer _registersContainer;
+        private readonly ushort _registerValue;
+
+        public SetIRegisterInstruction(RegistersContainer registersContainer, ushort registerValue)
+        {
+            _registersContainer = registersContainer;
+            _registerValue = registerValue;
+        }
+
+
+        public void Execute()
+        {
+            _registersContainer.WriteIRegister(_registerValue);
+        }
+    }
+}
