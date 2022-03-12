edc7ff29e7f0c839629c2bcec5e6f05cea39123f
JinnFighter <chernayamolnia@yandex.ru>
2022-03-12 14:11:10 +0300

    add: ClearScreenInstruction

--- Emulator/Display.cs
+++ Emulator/Display.cs
@@ -2,11 +2,15 @@
 {
     internal class Display
     {
+        public int Width { get; }
+        public int Height { get; }
         public bool[,] Pixels { get; }
 
         public Display()
         {
-            Pixels = new bool[64, 32];
+            Width = 64;
+            Height = 32;
+            Pixels = new bool[Width, Height];
         }
     }
 }
