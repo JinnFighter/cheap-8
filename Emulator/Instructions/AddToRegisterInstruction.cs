59c4329ef3f8e21bf57b996fb1709ea6724af288
JinnFighter <chernayamolnia@yandex.ru>
2022-03-12 15:14:18 +0300

    add: AddToRegisterInstruction

---
+++ Emulator/Instructions/AddToRegisterInstruction.cs
@@ -0,0 +1,21 @@
+﻿namespace Emulator.Instructions
+{
+    internal class AddToRegisterInstruction : IInstruction
+    {
+        private readonly RegistersContainer _registersContainer;
+        private readonly int _index;
+        private readonly byte _registerValue;
+
+        public AddToRegisterInstruction(RegistersContainer registersContainer, int index, byte registerValue)
+        {
+            _registersContainer = registersContainer;
+            _index = index;
+            _registerValue = registerValue;
+        }
+
+        public void Execute()
+        {
+            _registersContainer.Vs[_index] += _registerValue;
+        }
+    }
+}
